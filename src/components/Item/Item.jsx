9456b83eeaa7db74d3ae9ad1ccd9af9cee3be6ed
dhruvkumar1805 <dhruvkumar1805@gmail.com>
2024-03-13 19:21:07 +0530

    website: Update Item component
    Signed-off-by: dhruvkumar1805 <dhruvkumar1805@gmail.com>

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,17 +2,21 @@ import React from "react";
 
 function Item(props) {
   return (
-    <div className="w-[300px] cursor-pointer bg-gray-300 dark:bg-slate-400 p-2 rounded-xl hover:scale-105 transition-all duration-150">
-      <div className="overflow-hidden rounded-lg">
+    <div className="w-[350px] cursor-pointer bg-gray-100 dark:bg-gray-800 p-3 rounded-xl shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105">
+      <div className="overflow-hidden rounded-lg mb-3">
         <img
-          className="h-[300px] w-full object-cover"
+          className="h-[200px] w-full object-cover"
           src={props.image}
           alt={props.name}
         />
       </div>
-      <p className="mt-2 leading-5 truncate">{props.title}</p>
-      <div className="flex mt-2 space-x-4">
-        <div>${props.price}</div>
+      <p className="text-lg font-semibold text-gray-800 dark:text-gray-200 truncate">
+        {props.title}
+      </p>
+      <div className="flex items-center justify-between mt-2">
+        <div className="text-xl font-semibold text-gray-700 dark:text-gray-300">
+          ${props.price}
+        </div>
       </div>
     </div>
   );
